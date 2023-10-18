a0bef9eab5d03484ab0cfbb4917d83bdba20818c
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-10-18 17:23:14 +0300

    commit 3

--- laborator2/p3/src/main.rs
+++ laborator2/p3/src/main.rs
@@ -10,74 +10,111 @@ fn add_str(s: &mut String, cat: &str) {
     s.push_str(cat);
 }
 
-fn int_to_str(mut n: u32) -> String {
+fn int_to_str(mut n: i32) -> String {
     if n == 0 {
         return String::from("0");
     }
     let mut n_str: String = String::from("");
-    let mut m: u32 = 1;
-    while n >= m {
-        let mut n2: u32 = n;
-        let mut poz: u32 = 0;
-        while n2 > m * 10 {
-            n2 = n2 / 10;
-            poz += 1;
-        }
-        let cif: u32 = n2 % 10;
-        println!("{cif}");
-        let c = (cif as u8 + '0' as u8) as char;
-        n_str.push(c);
-        if poz % 3 == 0 {
-            n_str.push(' ');
-        }
-        m = m * 10;
+    if n < 0 {
+        n_str.push('-');
+        n *= -1;
     }
-
+    let mut exp: i32 = 1;
+    let mut poz: i32 = 0;
+    while n >= exp {
+        exp *= 10;
+        poz += 1;
+    }
+    while exp > 1 {
+        let cif: i32 = n % exp / (exp / 10);
+        let cif_char: char = (cif as u8 + '0' as u8) as char;
+        n_str.push(cif_char);
+        if poz % 3 == 1 && poz != 1 {
+            n_str.push('_');
+        }
+        poz -= 1;
+        exp /= 10;
+    }
     return n_str;
 }
 
-fn int_to_str_no_sep(mut n: u32) -> String {
+fn int_to_str_no_sep(mut n: i32) -> String {
     if n == 0 {
         return String::from("0");
     }
     let mut n_str: String = String::from("");
-    let mut m: u32 = 1;
-    while n >= m {
-        let mut n2: u32 = n;
-        let mut poz: u32 = 0;
-        while n2 > m * 10 {
-            n2 = n2 / 10;
-            poz += 1;
-        }
-        let cif: u32 = n2 % 10;
-        println!("{cif}");
-        let c = (cif as u8 + '0' as u8) as char;
-        n_str.push(c);
-        m = m * 10;
+    if n < 0 {
+        n_str.push('-');
+        n *= -1;
     }
-
+    let mut exp: i32 = 1;
+    while n >= exp {
+        exp *= 10;
+    }
+    while exp > 1 {
+        let cif: i32 = n % exp / (exp / 10);
+        let cif_char: char = (cif as u8 + '0' as u8) as char;
+        n_str.push(cif_char);
+        exp /= 10;
+    }
     return n_str;
 }
 
-fn add_integer(s: &mut String, mut n: u32) {
+fn add_integer(s: &mut String, n: i32) {
     s.push_str(&int_to_str(n));
 }
 
 fn add_float(s: &mut String, mut n: f32) {
+    if n < 0.0 {
+        n = n * (-1.0);
+        s.push('-');
+    }
     let int: i32 = n.floor() as i32;
     let mut fr: f32 = n - n.floor();
     while fr > fr.floor() {
         fr = fr * 10.0;
     }
-    add_integer(&mut s, int as u32);
+    s.push_str(&int_to_str_no_sep(int));
     s.push('.');
-    add_integer(&mut s, fr as u32);
+    s.push_str(&int_to_str_no_sep(fr as i32));
+}
+
+fn create_string() -> String {
+    let mut s = String::from("");
+    add_spaces(&mut s, 40);
+    add_str(&mut s, "I");
+    add_spaces(&mut s, 1);
+    add_str(&mut s, "💚\n");
+    add_spaces(&mut s, 40);
+    add_str(&mut s, "RUST\n");
+    add_spaces(&mut s, 4);
+    add_str(&mut s, "Most");
+    add_spaces(&mut s, 12);
+    add_str(&mut s, "crate");
+    add_spaces(&mut s, 6);
+    add_integer(&mut s, 306_437_968);
+    add_spaces(&mut s, 11);
+    add_str(&mut s, "and");
+    add_spaces(&mut s, 5);
+    add_str(&mut s, "latest");
+    add_spaces(&mut s, 9);
+    add_str(&mut s, "is\n");
+    add_spaces(&mut s, 9);
+    add_str(&mut s, "downloaded");
+    add_spaces(&mut s, 8);
+    add_str(&mut s, "has");
+    add_spaces(&mut s, 13);
+    add_str(&mut s, "downloads");
+    add_spaces(&mut s, 5);
+    add_str(&mut s, "the");
+    add_spaces(&mut s, 14);
+    add_str(&mut s, "version");
+    add_spaces(&mut s, 4);
+    add_float(&mut s, 2.038);
+    add_str(&mut s, ".\n");
+    return s;
 }
 
 fn main() {
-    let mut s = String::from("Hello");
-    add_float(&mut s, 1123.25);
-    print!("{s}");
-    // print!("{s}");
-    // print!("Done");
+    print!("{}", create_string());
 }
