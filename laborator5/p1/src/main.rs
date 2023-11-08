6adf21e0d3e9be2828948f2bc493251641256052
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-11-08 15:37:38 +0200

    laborator5

---
+++ laborator5/p1/src/main.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use std::fs;
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    phone: String,
+    age: u32,
+}
+
+fn main() -> Result<()> {
+    let mut people: Vec<Person> = Vec::new();
+    let data = fs::read_to_string("src/data.txt")?;
+    for line in data.lines() {
+        let mut i = 0;
+        let mut name: String = String::new();
+        let mut phone: String = String::new();
+        let mut age: u32 = 0;
+        for field in line.split(",") {
+            match i {
+                0 => name = field.to_string(),
+                1 => phone = field.to_string(),
+                _ => age = field.parse()?,
+            }
+            i += 1;
+        }
+        people.push(Person { name, phone, age })
+    }
+    let mut min = 0;
+    let mut max = 0;
+    for i in 2..people.len() {
+        if people[i].age > people[max].age {
+            max = i;
+        }
+        if people[i].age < people[min].age {
+            min = i;
+        }
+    }
+    println!("Person with minimum age: {:?}", people[min]);
+    println!("Person with minimum age: {:?}", people[max]);
+    Ok(())
+}
---
+++ laborator5/p3/src/main.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use serde_derive::Deserialize;
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+struct Person {
+    name: String,
+    phone: String,
+    age: u32,
+}
+fn main() -> Result<()> {
+    let mut people: Vec<Person> = Vec::new();
+    let data = fs::read_to_string("src/data.json")?;
+    for line in data.lines() {
+        println!("{line}");
+        let p: Person = serde_json::from_str(line)?;
+        people.push(p);
+    }
+    // println!("{}", people.len());
+    let mut min = 0;
+    let mut max = 0;
+    for i in 2..people.len() {
+        if people[i].age > people[max].age {
+            max = i;
+        }
+        if people[i].age < people[min].age {
+            min = i;
+        }
+    }
+    println!("Person with minimum age: {:?}", people[min]);
+    println!("Person with minimum age: {:?}", people[max]);
+    Ok(())
+}
