e6631dad47e159a58097eb64760bcfc15e1a1639
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-12-16 19:31:30 +0200

    Integrated commandline arguments
    Used the clap crate to recognise apropriate command line arguments

---
+++ project/redditor/src/args.rs
@@ -0,0 +1,39 @@
+pub use clap::{Parser, ValueEnum};
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Order {
+    New,
+    Hot,
+    Top,
+}
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about = "Execcutable that shows new posts from a subreddit after a set ammount of time has passed"
+)]
+
+pub struct Args {
+    ///The subreddit the data will be provided for
+    #[arg(short, long)]
+    subreddit: String,
+
+    ///The order the posts are listed in "hot" order
+    #[arg(short, long, value_enum,default_value_t = Order::Hot)]
+    order: Order,
+}
+
+impl Args {
+    pub fn parsed() -> Self {
+        Self::parse()
+    }
+    pub fn get_subreddit(&self) -> &str {
+        &self.subreddit
+    }
+    pub fn get_order(&self) -> &str {
+        match &self.order {
+            Order::Hot => return "hot",
+            Order::New => return "new",
+            Order::Top => return "top",
+        }
+    }
+}
--- project/redditor/src/main.rs
+++ project/redditor/src/main.rs
@@ -1,10 +1,15 @@
 mod Reddit;
+mod args;
+
 use anyhow::Result;
 use chrono::Local;
 use std::time::Duration;
 
 fn main() -> Result<()> {
-    let mut data = Reddit::SubredditUpdate::new("funny", "new");
+    let args = args::Args::parsed();
+    // println!("{:?}", args);
+
+    let mut data = Reddit::SubredditUpdate::new(args.get_subreddit(), args.get_order());
     loop {
         let current_time = Local::now().format("%H:%M:%S %d.%m.%Y");
         println!("Posts as of {}", current_time);
