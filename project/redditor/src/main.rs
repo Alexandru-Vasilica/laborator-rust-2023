6b1bbeb1ea32058c468b472484552bada81f294b
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2023-12-16 18:37:06 +0200

    Implemented loop for printing new posts

--- project/redditor/src/main.rs
+++ project/redditor/src/main.rs
@@ -1,8 +1,14 @@
 mod Reddit;
 use anyhow::Result;
+use chrono::Local;
+use std::time::Duration;
 
 fn main() -> Result<()> {
-    let mut data = Reddit::SubredditUpdate::new("cats", "hot");
-    data.update()?;
-    Ok(())
+    let mut data = Reddit::SubredditUpdate::new("funny", "new");
+    loop {
+        let current_time = Local::now().format("%H:%M:%S %d.%m.%Y");
+        println!("Posts as of {}", current_time);
+        data.update()?;
+        std::thread::sleep(Duration::from_secs(20));
+    }
 }
